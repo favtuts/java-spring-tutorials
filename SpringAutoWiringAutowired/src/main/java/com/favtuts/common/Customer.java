abd03a800f61b8980dba1bc5e399ba1a4039cfce
favtuts <favtuts@gmail.com>
2022-06-26 17:38:27 +0700

    demo Autowired annotation with three cases: setter method, constructor, field

--- SpringAutoWiringAutowired/src/main/java/com/favtuts/common/Customer.java
+++ SpringAutoWiringAutowired/src/main/java/com/favtuts/common/Customer.java
@@ -5,16 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class Customer 
 {
 	// you want autowired this field
+	// @Autowired field
+	// @Autowired
+	// To disable Dependency checking by setting the required attribute
+	@Autowired(required=false)
 	private Person person;
-	
-	//getter and setter methods
+		
 	private int type;
 	private String action;
+	
+	/*
+	//@Autowired construtor
+    @Autowired
+    public Customer(Person person) {
+        this.person = person;
+    }
+	*/
+    
+    //getter and setter methods
+    
 	public Person getPerson() {
 		return person;
 	}
 	
-	@Autowired
+	//@Autowired setter method
+	//@Autowired
 	public void setPerson(Person person) {
 		this.person = person;
 	}
