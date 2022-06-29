7f2a9ee803a7311463d99a6edfb97c78c07844b9
favtuts <favtuts@gmail.com>
2022-06-29 16:22:38 +0700

    complete Spring Auto scanning components

---
+++ Spring2-Auto-Scan/src/main/java/com/favtuts/customer/services/CustomerService.java
@@ -0,0 +1,19 @@
+package com.favtuts.customer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.favtuts.customer.dao.CustomerDAO;
+
+@Service
+public class CustomerService 
+{
+	@Autowired
+	CustomerDAO customerDAO;
+
+	@Override
+	public String toString() {
+		return "CustomerService [customerDAO=" + customerDAO + "]";
+	}
+		
+}
---
+++ SpringAutoScanComponent/README.md
@@ -0,0 +1,11 @@
+# Spring Auto scanning components
+
+* https://www.favtuts.com/spring-auto-scanning-components/
+
+# How to start
+
+```bash
+$ git clone https://github.com/favtuts/java-spring-tutorials.git
+
+$ cd SpringAutoScanComponent
+```
